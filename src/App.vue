60702024b8be6faf3912e021767bca0f2b5f0440
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-02-21 00:12:07 -0300

    ADD: initial structure and Table component

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import Table from './components/Table.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="col-sm-8 col-sm-offset-2">
+      <h1>Titulo de App.vue</h1>
+      <Table />
+    </div>
+</div>
+</template>
+
+<style scoped>
+ body {
+  background-color: antiquewhite;
+ }
+ h1 {
+  color: red;
+ }
+ 
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,50 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Checkmark Done</th>
+                <th>Task Name</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="task in tasks">
+                <td>
+                    <input type="checkbox">
+                </td>
+                <td>
+                    {{ task.titulo }}
+                </td>
+                <td>
+                    <button>DELETE</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>          
+</template>
+
+<script>
+    export default {
+        name: 'tasks',
+        data() {
+            return {
+                tasks: [
+                    {
+                        titulo: 'task 1'
+                    },
+                    {
+                        titulo: 'task 2'
+                    },
+                    {
+                        titulo: 'task 3'
+                    }
+                    
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
+
